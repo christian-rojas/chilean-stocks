a0c397062d3d8897b9fe5c99248f79a1ce734551
christian-rojas <christian.ici17@gmail.com>
2023-06-17 23:22:24 -0400

    add github action

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const { getStockData } = require("./utils.js");
 
 exports.helloWorld = async (req, res) => {    
     const stockName = req.body.nemo
-    const stockData = await getStockData(stockName)    
-    sendMessage(stockName, stockData);
-    res.send("ok")
+    const nemos = ['itaucl', 'chile', 'enelchile', 'sqm', 'cap']
+    const data = nemos.map(async (nemo) => {
+        return getStockData(nemo)
+    })
+    const stockData = await Promise.allSettled(data)
+    
+    let index = 0;
+    for(let item of stockData){
+        if(item.status == 'fulfilled') {
+            sendMessage(nemos[index], item.value);
+        }
+        index++;
+        continue;
+    }
+    console.info("message published successfully")
+    res.send("done")
 };
